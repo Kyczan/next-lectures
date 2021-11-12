e20f5439a47e13342a472aab69c76b2e0f92d840
Krystian Kowal <krystian@kowal.pro>
2021-11-12 22:26:22 +0100

    :white_check_mark: (db): Restore process.env between tests

--- src/utils/db/dbConnect.test.ts
+++ src/utils/db/dbConnect.test.ts
@@ -6,24 +6,32 @@ import mongoose from 'mongoose'
 import dbConnect from './dbConnect'
 
 describe('dbConnect', () => {
-  it('throws error when no db url provided', async () => {
-    await expect(dbConnect()).rejects.toThrow(
-      'Please define the MONGODB_URI environment variable inside .env.local'
-    )
+  const OLD_ENV = process.env
+
+  beforeEach(() => {
+    jest.resetModules()
+    process.env = { ...OLD_ENV }
+  })
+
+  afterAll(() => {
+    process.env = OLD_ENV
   })
 
   it('returns connection when url provided', async () => {
     const mongooseConnectMockFn = jest.fn().mockResolvedValueOnce({})
     mongoose.connect = mongooseConnectMockFn
-    process.env = {
-      ...process.env,
-      MONGODB_URI: 'test',
-    }
+    process.env.MONGODB_URI = 'test'
 
     await dbConnect()
     // second invoke should use existing connection
     await dbConnect()
 
     expect(mongooseConnectMockFn).toHaveBeenCalledTimes(1)
+  })
+
+  it('throws error when no db url provided', async () => {
+    await expect(dbConnect()).rejects.toThrow(
+      'Please define the MONGODB_URI environment variable inside .env.local'
+    )
   })
 })
